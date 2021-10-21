b2bb620294ca1dfed6d57c81aa6326f579c05862
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-10-21 11:15:27 -0400

    added exact route for home route and implemented withRouter HOC into the searchbar to navigate the user to results route

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,27 +1,22 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import reducers from './reducers';
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
-
 import Home from './components/home';
-
-
+import Results from './components/results';
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
         <Switch>
-            <Route path='/' component={Home}/>
-            <Route path='/results' component={Home}/>
+          
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
 import { Field, reduxForm} from 'redux-form';
 
+import { withRouter } from 'react-router-dom';
+
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
         console.log('trying to handle submit for query', query);
         //navigate to a new route.
+        this.props.history.push('/results');
     }
 
     renderInput(field) {
         return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
-
     render() {
-
         const { handleSubmit } = this.props;
-
         return (
-            
-            
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <Field name="query" component={this.renderInput}/>
             </form>
         )
     }
 }
-
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
