6bc249388e1af79e14242ca829a7725d3bc666a2
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-10-23 10:30:28 -0400

    added mapStateToProps(state){} in the recentPosts.js component and rendered the

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+class RecentPosts extends Component {
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
 
-class SearchBar extends Component {
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+                return (
+                    <li key={index}>
+                        {post.title}
+                    </li>
+                )   
+            }
+        })
+        return posts
+    }
+
     render() {
         return (
             <div className="recent-posts">
@@ -10,6 +28,7 @@ class SearchBar extends Component {
                         <li>recent post 0</li>
                         <li>recent post 1</li>
                         <li>recent post 2</li>
+                        {this.renderPosts()}
                     </ul>
                 </div>
 
@@ -18,4 +37,11 @@ class SearchBar extends Component {
     }
 }
 
-export default SearchBar;
+ 
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
